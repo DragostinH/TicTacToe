1a5334c773cee04f4e8e70e5bf01ba8e88ef272c
DragostinH <dragostin.hristov@gmail.com>
2022-01-08 23:14:43 +0200

    added displayController along with using the 2 players

--- app.js
+++ app.js
@@ -7,27 +7,63 @@ const gameBoard = (() => {
 
     const cells = document.getElementsByClassName('squares');
     const cellsArray = Array.from(cells);
-    cellsArray.forEach(e=>{e.addEventListener('click',()=>{
-        console.log(`you clicked ${e.textContent}`);
-    })})
-    console.log(cells);
-    console.log(cellsArray);
 
-    for (let i = 0; i < multiArray.length; i++) {
-        let counter = 0;
-        for (let j = 0; j < multiArray[i].length; j++) {
-            multiArray[i][j] = cellsArray[counter]; 
-        }        
+
+
+    const addDivsToArray = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[i].length; j++) {
+                arr[i][j] = '';
+            }
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            let counter = 0;
+            for (let j = 0; j < arr[i].length; j++) {
+                arr[i][j] = cellsArray[counter]
+                cellsArray[counter].id = counter;
+            }
+        }
+
+        return arr;
     }
 
-    return { multiArray };
+    return { multiArray, cellsArray, addDivsToArray };
 
 
 
 })();
 
-console.log(gameBoard.multiArray);
+const playerFactory = (name, symbol) => {
+    return { name, symbol };
+}
+
+const displayController = (() => {
+    const playerOne = playerFactory('jeff', 'X');
+    const playerTwo = playerFactory('tom', 'O');
+    let twoDimArray = gameBoard.multiArray;
+    let currentPlayer;
+    let numberOfTurns = 4;
+
+    gameBoard.cellsArray.forEach(e => e.addEventListener('click', () => {
+        if (numberOfTurns % 2 === 0) {
+            e.textContent = playerOne.symbol
+            numberOfTurns--
+            gameBoard.addDivsToArray(twoDimArray);
+            console.log(twoDimArray);
+        } else {
+            numberOfTurns--
+            e.textContent = playerTwo.symbol;
+        }
+
+
+        // numberOfTurns % 2 === 0 ? e.textContent = playerOne.symbol : e.textContent = playerTwo.symbol;
+
+    }))
+
+
+})();
 
 
 
-// game starts -> board is loaded -> players loaded -> P1 turn -> click on square -> event fires -
+
